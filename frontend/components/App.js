78458a2630613916869149bd13d88ada093aafa6
ashurik522 <ashurik522@gmail.com>
2022-06-06 15:06:32 -0400

    intial commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,21 @@
 import React from 'react'
 
+const toDos = [
+  {
+    name: '',
+    id: null,
+    comleted: false
+  }
+]
+
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      toDos: toDos 
+    }
+  }
+
   render() {
     return (
       <div>
